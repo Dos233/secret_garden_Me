97fc2f985308af3562ee9653e2db841d4a66371b
Lossy <48687924+Dos233@users.noreply.github.com>
2020-07-23 16:39:38 +0800

    Update register_user.php

--- php/register_user.php
+++ php/register_user.php
@@ -27,22 +27,21 @@ else
 }
 
 try{
-    //服务器配置
-    $mail->CharSet ="UTF-8";                     //设定邮件编码
-    $mail->SMTPDebug = 0;                        // 调试模式输出
-    $mail->isSMTP();                             // 使用SMTP
-    $mail->Host = 'sg2plcpnl0234.prod.sin2.secureserver.net';                // SMTP服务器
-    $mail->SMTPAuth = true;                      // 允许 SMTP 认证
-    $mail->Username = 'noreply@lossyhome.xyz';                // SMTP 用户名  即邮箱的用户名
-    $mail->Password = 'qaz147';             // SMTP 密码  部分邮箱是授权码(例如163邮箱)
-    $mail->SMTPSecure = 'ssl';                    // 允许 TLS 或者ssl协议
-    $mail->Port = 465;                            // 服务器端口 25 或者465 具体要看邮箱服务器支持
+    $mail->CharSet ="UTF-8";                     
+    $mail->SMTPDebug = 0;                        
+    $mail->isSMTP();                             
+    $mail->Host = 'sg2plcpnl0234.prod.sin2.secureserver.net';                
+    $mail->SMTPAuth = true;                      
+    $mail->Username = 'noreply@lossyhome.xyz';                
+    $mail->Password = 'qaz147';             
+    $mail->SMTPSecure = 'ssl';                    
+    $mail->Port = 465;                            
 
-    $mail->setFrom('noreply@lossyhome.xyz', 'MY.Garden');  //发件人
-    $mail->addAddress($email);  // 收件人
+    $mail->setFrom('noreply@lossyhome.xyz', 'MY.Garden');  
+    $mail->addAddress($email);  
    
     //Content
-    $mail->isHTML(true);                                  // 是否以HTML文档格式发送  发送后客户端可直接显示对应HTML内容
+    $mail->isHTML(true);                                  
     $mail->Subject = 'Your email have registered successfully!';
     $mail->Body    = '<h1>You have registered !</h1>'.'<div>Your email: <div>'.$email.'<p><p>' .'<div>Registered date: <div>'. date('Y-m-d H:i:s');
 
